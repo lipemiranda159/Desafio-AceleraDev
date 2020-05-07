321c13173fc8f2c1a9c59faedd492dd3cc39115d
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-06 23:27:42 -0300

    Create models project

---
+++ Condenation.AceleraDev.Domain/Models/Response.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Condenation.AceleraDev.Domain.Models
+{
+    public class Response
+    {
+        [JsonProperty("numero_casas")]
+        public long NumeroCasas { get; set; }
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("cifrado")]
+        public string Cifrado { get; set; }
+
+        [JsonProperty("decifrado")]
+        public string Decifrado { get; set; }
+
+        [JsonProperty("resumo_criptografico")]
+        public string ResumoCriptografico { get; set; }
+    }
+}
