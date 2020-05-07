30b0fe64d9179048365117cbd61485593cf98ce8
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-06 23:25:13 -0300

    Create interface

---
+++ Condenation.AceleraDev.Domain/Interfaces/ICryptographyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Condenation.AceleraDev.Domain.Interfaces
+{
+    public interface ICryptographyService
+    {
+        string Cript(string input);
+        string Decript(string input);
+    }
+}
