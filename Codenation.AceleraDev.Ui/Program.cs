a488f5242f6d846ce9f1c8584a3947ae18d43591
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-06 22:59:24 -0300

    create project

---
+++ Codenation.AceleraDev.Ui/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Codenation.AceleraDev.Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((char)("abc"[0] + 1));
+            Console.ReadLine();
+        }
+    }
+}
