dd892a26727c91c03ea5e90a7faab3ded00aaaaf
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-06 23:25:45 -0300

    Create service api

---
+++ Codenation.AceleraDev.Services/IAceleraDevApiService.cs
@@ -0,0 +1,24 @@
+﻿using Condenation.AceleraDev.Domain.Models;
+using RestEase;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Codenation.AceleraDev.Services
+{
+    public interface IAceleraDevApiService
+    {
+        /// <summary>
+        /// Get data from api
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [Get("generate-data")]
+        Task<Response> GetDataAsync([Query]string token);
+
+        [Header("Content-Disposition", "form-data; filename=\"answer.json\"")]
+        [Header("Content-Type", "text/plain")]
+        [Post("submit-solution")]
+        Task UploadFileVersionOneAsync([Body] Stream file, [Query] string token);
+    }
+}
